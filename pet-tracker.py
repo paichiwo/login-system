f6c6cb53e5738c9e8bb69d72a7432ae1d386c6b5
paichiwo <lzerucha@gmail.com>
2023-05-09 12:29:24 +0100

    Create basic skeleton with ideas for pet-care-tracker application

---
+++ pet-tracker.py
@@ -0,0 +1,26 @@
+# ## PET TRACKER
+#
+# ### Overview
+#
+# *   The app is designed to help pet owners keep track of their pet's care and well-being.
+# *   Users can create a profile for their pet(s) and record information such as breed, age, weight, and medical history.
+#
+# ### Features
+#
+# *   Information profile: Displays pet profile with info: breed, weight, age (in human years too).
+# *   Feeding schedule: Users can set up a feeding schedule for their pet(s) and **receive reminders** when it's time to feed them.
+# *   Medication tracker: Users can track their pet's medication schedules and **receive reminders** when it's time for a dose. 
+# *   Appointment reminders: Users can schedule appointments for their pet(s) and receive reminders in advance.
+#
+# ### Additional features (optional)
+#
+# *   Pet-friendly places: Users can search for pet-friendly parks, restaurants, and other locations near them.
+# *   Lost pet alert: Users can create a profile for their pet and receive alerts if they are reported missing in the area.
+# *   Social network: Users can connect with other pet owners and share tips and advice.
+#
+# ### Technical specifications
+#
+# *   The app will be available for iOS and Android devices.
+# *   The app will use a secure database to store user and pet information.
+# *   The app will require access to the user's location to provide relevant information about pet-friendly locations.
+# *   The app will use push notifications to remind users about appointments, feeding schedules, and other important events.
