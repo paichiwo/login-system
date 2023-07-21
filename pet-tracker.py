1f274cb37e5c31a35cba2f75e4aad6f0f2749764
paichiwo <lzerucha@gmail.com>
2023-07-21 19:19:48 +0100

    Create call for creating a database in pet-tracker.py

--- pet-tracker.py
+++ pet-tracker.py
@@ -1,31 +1,3 @@
-from flask import Flask, render_template, request, redirect
-from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime
-from PIL import Image
+from src.helpers import create_database
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route('/profile', methods=['GET', 'POST'])
-def profile():
-    if request.method == 'POST':
-        name = request.form['name']
-        breed = request.form['breed']
-        age = int(request.form['age'])
-        weight = float(request.form['weight'])
-
-        # Store the pet's information in a database or file
-        # (you can use SQLite, JSON, or any other suitable option)
-
-        return redirect('/')
-
-    return render_template('profile.html')
-
-
-if __name__ == '__main__':
-    app.run()
+create_database('db/db.db', 'db/db.sql')
