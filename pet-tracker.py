9fbbb095f8107f0f9ec60e1338509535710146ee
paichiwo <lzerucha@gmail.com>
2023-08-23 20:26:31 +0100

    Continue implementing main logic

--- pet-tracker.py
+++ pet-tracker.py
@@ -1,11 +1,11 @@
 import os
 from src.helpers import create_database
 from src.config import SQL_FILEPATH, DB_FILEPATH
-
+from src.login import login_window
 
 if not os.path.exists(DB_FILEPATH) and os.path.exists(SQL_FILEPATH):
     create_database(DB_FILEPATH, SQL_FILEPATH)
     print("New database successfully created")
 else:
     print("Database exists")
-
+login_window()
