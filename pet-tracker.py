896cb7ce22e1766ffe79595cb0a9014dea9be2b0
paichiwo <lzerucha@gmail.com>
2023-08-23 19:23:54 +0100

    Implement logic for creating db

--- pet-tracker.py
+++ pet-tracker.py
@@ -1,3 +1,10 @@
 from src.helpers import create_database
+from src.config import sql_file_path, db_file_path
+import os
 
-create_database('db/db.db', 'db/db.sql')
+if not os.path.exists(db_file_path) and os.path.exists(sql_file_path):
+    create_database('db/db.db', 'db/db.sql')
+    print("New database successfully created")
+else:
+    print("Database already exists")
+
