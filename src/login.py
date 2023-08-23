fd2ac7f63a32df6099a840d42728107b2fdaf5b5
paichiwo <lzerucha@gmail.com>
2023-08-23 20:25:55 +0100

    Put login window logic into login_window func Add docstrings and comments

--- src/login.py
+++ src/login.py
@@ -2,40 +2,44 @@ import tkinter as tk
 import customtkinter as ctk
 from tkinter import messagebox
 
-root = ctk.CTk()
-root.geometry("400x600")
-root.title("Login Page")
 
+def login_window():
+    """Create a login window using CTK"""
+    root = ctk.CTk()
+    root.geometry("400x600+400+100")
+    root.title("Login Page")
 
-def login():
-    username = "admin"
-    password = "123"
+    def login():
+        """Login button callback"""
+        username = "admin"
+        password = "123"
 
-    if user_entry.get() == username and user_pass.get() == password:
-        messagebox.showinfo(title="Login Successful", message="You have logged in")
-    else:
-        messagebox.showerror(title="Login Failed", message="Invalid credentials")
+        if user_entry.get() == username and user_pass.get() == password:
+            messagebox.showinfo(title="Login Successful", message="You have logged in")
+        else:
+            messagebox.showerror(title="Login Failed", message="Invalid credentials")
 
+    # Create UI elements
+    label = ctk.CTkLabel(root, text="Pet Health Tracker Logo")
+    label.pack(pady=20)
 
-label = ctk.CTkLabel(root, text="This is the main UI page")
-label.pack(pady=20)
+    # Frame and its contents
+    frame = ctk.CTkFrame(master=root)
+    frame.pack(pady=20, padx=40, fill='both', expand=True)
 
-frame = ctk.CTkFrame(master=root)
-frame.pack(pady=20, padx=40, fill='both', expand=True)
+    label = ctk.CTkLabel(master=frame, text='Login frame')
+    label.pack(pady=12, padx=10)
 
-label = ctk.CTkLabel(master=frame, text='Modern Login System UI')
-label.pack(pady=12, padx=10)
+    user_entry= ctk.CTkEntry(master=frame, placeholder_text="Username")
+    user_entry.pack(pady=12, padx=10)
 
-user_entry= ctk.CTkEntry(master=frame, placeholder_text="Username")
-user_entry.pack(pady=12, padx=10)
+    user_pass= ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
+    user_pass.pack(pady=12, padx=10)
 
-user_pass= ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
-user_pass.pack(pady=12, padx=10)
+    button = ctk.CTkButton(master=frame, text='Login', command=login)
+    button.pack(pady=12, padx=10)
 
-button = ctk.CTkButton(master=frame, text='Login', command=login)
-button.pack(pady=12, padx=10)
+    checkbox = ctk.CTkCheckBox(master=frame, text='Remember Me')
+    checkbox.pack(pady=12, padx=10)
 
-checkbox = ctk.CTkCheckBox(master=frame, text='Remember Me')
-checkbox.pack(pady=12, padx=10)
-
-root.mainloop()
+    root.mainloop()
