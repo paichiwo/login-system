fa22d349be3ac30c4f3182dc4cbd6086a1f259ac
paichiwo <lzerucha@gmail.com>
2023-08-25 19:41:03 +0100

    Implement center_window func

--- src/helpers.py
+++ src/helpers.py
@@ -25,6 +25,18 @@ def create_database(database_path, sql_file_path):
     conn.close()
 
 
+def center_window(window, width, height):
+    """Center a window on the screen using the provided dimensions"""
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+
+    window.geometry(f"{width}x{height}+{x}+{y}")
+    window.update_idletasks()
+
+
 def is_valid_email(email):
     """Check for a valid email address format"""
     email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
