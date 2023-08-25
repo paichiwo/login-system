f79487bb8fd0c62966977959b2144af7424b3b89
paichiwo <lzerucha@gmail.com>
2023-08-25 22:08:51 +0100

    Implement SignUpWindow class.
    1.Enters data into database
    2.Checks for errors in input data

--- src/login_window.py
+++ src/login_window.py
@@ -1,8 +1,9 @@
+import sqlite3
+
 import customtkinter as ctk
 from tkinter import BOTTOM
 from src.app_window import AppWindow
-from src.signup_window import SignUpWindow
-from src.helpers import center_window, is_valid_email
+from src.helpers import center_window, is_valid_email, create_user, is_valid_password
 
 
 class LoginWindow(ctk.CTk):
@@ -67,7 +68,6 @@ class LoginWindow(ctk.CTk):
 
     def login(self):
         """Login button callback"""
-
         username = "admin@test.com"
         password = "123"
 
@@ -87,5 +87,92 @@ class LoginWindow(ctk.CTk):
     def signup(self):
         """Sign Up button callback"""
         self.destroy()
-        signup = SignUpWindow()
-        signup.mainloop()
+        signup_window = SignUpWindow()
+        signup_window.mainloop()
+
+
+class SignUpWindow(ctk.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Window setup
+
+        self.title("Sign Up Window")
+        center_window(self, 300, 500)
+
+        # UI Elements
+
+        self.root_label = ctk.CTkLabel(
+            self,
+            text="pyPETS",
+            font=("Any", 26, "bold"))
+        self.root_label.pack(pady=20)
+
+        self.frame = ctk.CTkFrame(master=self)
+        self.frame.pack(pady=20, padx=20, fill='both', expand=True)
+
+        self.frame_top_label = ctk.CTkLabel(
+            master=self.frame,
+            text='Sign up')
+        self.frame_top_label.pack(pady=12, padx=10)
+
+        self.user_entry = ctk.CTkEntry(
+            master=self.frame,
+            placeholder_text="Email",
+            width=200)
+        self.user_entry.pack(pady=12, padx=10)
+
+        self.user_pass = ctk.CTkEntry(
+            master=self.frame,
+            placeholder_text="Password",
+            show="*",
+            width=200)
+        self.user_pass.pack(pady=12, padx=10)
+
+        self.signup_button = ctk.CTkButton(
+            master=self.frame,
+            text='Create Account',
+            width=200,
+            command=self.signup)
+        self.signup_button.pack(pady=12, padx=10)
+
+        self.message_label = ctk.CTkLabel(
+            self.frame,
+            text="Enter email and password",
+            text_color="grey")
+        self.message_label.pack(pady=12, padx=10)
+
+        self.cancel_button = ctk.CTkButton(
+            master=self.frame,
+            text='Cancel',
+            width=200,
+            command=self.cancel)
+        self.cancel_button.pack(pady=24, padx=10, side=BOTTOM)
+
+    def signup(self):
+        email = self.user_entry.get()
+        password = self.user_pass.get()
+
+        if is_valid_email(email):
+            if len(password) >= 6:
+                if is_valid_password(password):
+                    try:
+                        create_user(email, password)
+                    except sqlite3.Error as error:
+                        self.message_label.configure(text=f"SQLite Error: {error}")
+                    self.message_label.configure(text="Account created\nYou can now log in")
+                else:
+                    specials = "!@#$%^&*()-_=+{}[]|:;<>,.?"
+                    self.message_label.configure(text=f"Error\nAllowed characters:\n\n(A-Z) (a-z) (0-9)\n{specials}")
+            else:
+                self.message_label.configure(text="Password must be minimum 6 characters")
+        else:
+            self.message_label.configure(text="Please enter valid email address")
+
+    def cancel(self):
+        self.destroy()
+        login_window = LoginWindow()
+        login_window.mainloop()
+
+    def delete_invalid_password_character(self):
+        pass
