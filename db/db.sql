93f91758c15a8f579a5fedf6f3e3d8040a60dbbd
paichiwo <lzerucha@gmail.com>
2023-08-25 19:22:35 +0100

    username removed from user profile (using only email)

--- db/db.sql
+++ db/db.sql
@@ -3,7 +3,6 @@ BEGIN TRANSACTION;
 -- Create User Profile table
 CREATE TABLE user_profile (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL
 );
