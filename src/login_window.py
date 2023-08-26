5eb8862c122d042eda69e7c480a4ce3808a910f3
paichiwo <lzerucha@gmail.com>
2023-08-26 20:25:20 +0100

    Improve readability

--- src/login_window.py
+++ src/login_window.py
@@ -15,13 +15,13 @@ class LoginApp(ctk.CTk):
         center_window(self, 300, 500)
 
         self.frame = None
-        self.frame_top_label = None
+        self.frame_label = None
         self.user_entry = None
         self.user_pass = None
         self.confirm_user_pass = None
         self.login_button = None
         self.remember_me = None
-        self.message_label = None
+        self.msg_label = None
         self.signup_button = None
         self.cancel_button = None
 
@@ -31,15 +31,15 @@ class LoginApp(ctk.CTk):
         self.login_frame()
 
     def login_frame(self):
-        """Create UI elements for the login page"""
+        """Create UI elements for the login frame"""
         if self.frame:
             self.frame.destroy()
 
         self.frame = ctk.CTkFrame(master=self)
         self.frame.pack(pady=20, padx=20, fill='both', expand=True)
 
-        self.frame_top_label = ctk.CTkLabel(master=self.frame, text='Login')
-        self.frame_top_label.pack(pady=12, padx=10)
+        self.frame_label = ctk.CTkLabel(master=self.frame, text='Login')
+        self.frame_label.pack(pady=12, padx=10)
 
         self.user_entry = ctk.CTkEntry(master=self.frame, placeholder_text="Email", width=200)
         self.user_entry.pack(pady=12, padx=10)
@@ -53,22 +53,22 @@ class LoginApp(ctk.CTk):
         self.remember_me = ctk.CTkCheckBox(master=self.frame, text='Remember Me')
         self.remember_me.pack(pady=12, padx=10)
 
-        self.message_label = ctk.CTkLabel(self.frame, text="")
-        self.message_label.pack(pady=12, padx=10)
+        self.msg_label = ctk.CTkLabel(self.frame, text="")
+        self.msg_label.pack(pady=12, padx=10)
 
         self.signup_button = ctk.CTkButton(master=self.frame, text='Sign Up', width=200, command=self.signup)
         self.signup_button.pack(pady=24, padx=10, side=BOTTOM)
 
     def signup_frame(self):
-        """Create UI elements for the user registration page"""
+        """Create UI elements for the user registration frame"""
         if self.frame:
             self.frame.destroy()
 
         self.frame = ctk.CTkFrame(master=self)
         self.frame.pack(pady=20, padx=20, fill='both', expand=True)
 
-        self.frame_top_label = ctk.CTkLabel(master=self.frame, text='Sign up')
-        self.frame_top_label.pack(pady=12, padx=10)
+        self.frame_label = ctk.CTkLabel(master=self.frame, text='Sign up')
+        self.frame_label.pack(pady=12, padx=10)
 
         self.user_entry = ctk.CTkEntry(master=self.frame, placeholder_text="Email", width=200)
         self.user_entry.pack(pady=12, padx=10)
@@ -76,16 +76,16 @@ class LoginApp(ctk.CTk):
         self.user_pass = ctk.CTkEntry(master=self.frame, placeholder_text="Password", show="*", width=200)
         self.user_pass.pack(pady=12, padx=10)
 
-        self.confirm_user_pass = ctk.CTkEntry(master=self.frame, placeholder_text="Confirm Password", show="*", width=200)
+        self.confirm_user_pass = ctk.CTkEntry(master=self.frame, placeholder_text="Confirm", show="*", width=200)
         self.confirm_user_pass.pack(pady=12, padx=10)
 
-        self.signup_button = ctk.CTkButton(master=self.frame, text='Create Account', width=200, command=self.create_user)
+        self.signup_button = ctk.CTkButton(master=self.frame, text='Create Account', width=200, command=self.add_user)
         self.signup_button.pack(pady=12, padx=10)
 
-        self.message_label = ctk.CTkLabel(self.frame, text="Enter email and password", text_color="grey")
-        self.message_label.pack(pady=12, padx=10)
+        self.msg_label = ctk.CTkLabel(self.frame, text="Enter email and password", text_color="grey")
+        self.msg_label.pack(pady=12, padx=10)
 
-        self.cancel_button = ctk.CTkButton(master=self.frame, text='Go Back', width=200, command=self.cancel)
+        self.cancel_button = ctk.CTkButton(master=self.frame, text='Go Back', width=200, command=self.go_back)
         self.cancel_button.pack(pady=24, padx=10, side=BOTTOM)
 
     def login(self):
@@ -99,16 +99,16 @@ class LoginApp(ctk.CTk):
                 app = AppWindow()
                 app.mainloop()
             else:
-                self.message_label.configure(text="Wrong credentials")
+                self.msg_label.configure(text="Wrong credentials")
         else:
-            self.message_label.configure(text="Enter valid email address and password", text_color="grey")
+            self.msg_label.configure(text="Enter valid email address and password", text_color="grey")
 
     def signup(self):
-        """Sign Up button callback"""
+        """Show Sign Up frame"""
         self.signup_frame()
 
-    def create_user(self):
-        """Create user in the database"""
+    def add_user(self):
+        """Add a user to the database"""
         email = self.user_entry.get()
         password = self.user_pass.get()
         password_confirmation = self.confirm_user_pass.get()
@@ -120,17 +120,18 @@ class LoginApp(ctk.CTk):
                         try:
                             Database().create_user(email, password)
                         except sqlite3.Error as error:
-                            self.message_label.configure(text=f"SQLite Error: {error}")
-                        self.message_label.configure(text="Account created\nYou can now log in")
+                            self.msg_label.configure(text=f"SQLite Error: {error}")
+                        self.msg_label.configure(text="SUCCESS !\nGo back to log in")
                     else:
-                        specials = "!@#$%^&*()-_=+{}[]|:;<>,.?"
-                        self.message_label.configure(text=f"Error\nAllowed characters:\n\n(A-Z) (a-z) (0-9)\n{specials}")
+                        specials = "! @ # $ & - _"
+                        self.msg_label.configure(text=f"Allowed characters:\nuppercase, lowercase, digits, {specials}")
                 else:
-                    self.message_label.configure(text="Password must be minimum 6 characters")
+                    self.msg_label.configure(text="Password must be minimum 6 characters")
             else:
-                self.message_label.configure(text="Please enter valid email address")
+                self.msg_label.configure(text="Please enter valid email address")
         else:
-            self.message_label.configure(text="Passwords don't match")
+            self.msg_label.configure(text="Passwords don't match")
 
-    def cancel(self):
+    def go_back(self):
+        """Show login frame"""
         self.login_frame()
