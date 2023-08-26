14bbf8f815fa126f60e7c44892963cb3140dab50
paichiwo <lzerucha@gmail.com>
2023-08-26 20:26:01 +0100

    Change order, db closes at the end of flow

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ def main():
     """Initialize database and create a login window"""
     if not os.path.exists('./db/db.db'):
         Database().create_db()
-        Database().close_db()
+
         print("database created")
     else:
         print("database exists, proceeding...")
     login_app = LoginApp()
     login_app.mainloop()
+    Database().close_db()
 
 
 if __name__ == "__main__":
