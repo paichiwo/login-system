178d4349a2aeb23ec31c829a2acbc75be571d0f8
paichiwo <lzerucha@gmail.com>
2023-08-26 18:05:07 +0100

    Refactor for readability

--- src/app_window.py
+++ src/app_window.py
@@ -6,12 +6,8 @@ class AppWindow(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Window setup
-
         self.title("App Window")
-        center_window(self, 800, 600)
-
-        # UI elements
+        center_window(self, 200, 200)
 
         self.label = ctk.CTkLabel(self, text="Login Successful\nThis is main app window")
         self.label.pack(pady=12, padx=10)
